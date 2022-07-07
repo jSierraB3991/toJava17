5eedadde6ad7a7001133063751cecf64e81c6e1f
Juan David Sierra <juan.sierra@zabud.com.co>
2022-07-06 21:06:21 -0500

    basic project for java 11

---
+++ toJava11/src/test/java/com/example/toJava11/ToJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toJava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
